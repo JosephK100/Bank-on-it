189a88700128ffa441828ea94481be82e6c3f93d
Joseph Kloepper <Thunderpop57@Gmail.com>
2026-03-20 02:12:40 +0000

    It complies at long last.

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -6,9 +6,6 @@ public class CheckingAccount implements HasMenu{
 	public CheckingAccount(){
 		this.balance = 0.0;
 	}
-	private CheckingAccount(){
-		this.balance = 0.0
-	}
 	public CheckingAccount(double balance){
 		this.balance = balance;
 	}
@@ -22,7 +19,7 @@ public class CheckingAccount implements HasMenu{
 		System.out.println("1) Check balance");
 		System.out.println("2) Make a deposit");
 		System.out.println("3) Make a withdrawl");
-		System.out.print("Please enter 0-3");
+		System.out.print("Please enter 0-3: ");
 		Scanner sc = new Scanner(System.in);
 		return sc.nextLine();
 	}
@@ -38,37 +35,39 @@ public class CheckingAccount implements HasMenu{
 			} else if (choice.equals("3")){
 				makeWithdrawal();
 			} else {
-				System.out.println("Invlaid choice.")
+				System.out.println("Invalid choice.");
 		}
-	}
+	    }
 	}
 	public double getBalance(){
 		return balance;
 	}
 	public void makeDeposit(){
 		System.out.println("Making a deposit...");
-		System.out.print("How much would you like to deposit?");
+		System.out.print("How much would you like to deposit? ");
 		double amt = getDouble();
-		blance += amt;
+		balance += amt;
 		System.out.println("New balance: " + getBalance());
 	}
 	public void makeWithdrawal(){
-		System.out.print;n("Making a withdrawal...");
-		System.out.print("How much would you like to withdraw?");
+		System.out.println("Making a withdrawal...");
+		System.out.print("How much would you like to withdraw? ");
 		double amt = getDouble();
 		balance -= amt;
 		System.out.println("New balance: " + getBalance());
 	}
 	private double getDouble(){
 		Scanner sc = new Scanner(System.in);
+		try {
+			return Double.parseDouble(sc.nextLine());
+		} catch (Exception e) {
+			return 0.0;
+		}
+	}
 
 	public String getBalanceString(){
 		return String.format("$%.2f", balance);
-		try{
-			return Double.parseDouble(sc.nextLine());
-		} catch (Exception e){
-			return 0.0;
-		}
+	
 	}
 	public void setBalance(double balance){
 		this.balance = balance;
@@ -77,5 +76,4 @@ public class CheckingAccount implements HasMenu{
 		System.out.println("Checking balance...");
 		System.out.println("Current balance: " + getBalanceString());
 	}
-	private double getDouble(){
-		//Lost on this part
+}
