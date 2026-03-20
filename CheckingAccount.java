4abb19c0424e27c685890250b12490f09d96b232
Joseph Kloepper <Thunderpop57@Gmail.com>
2026-03-20 22:40:07 +0000

    Final changes for week 1

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
 public class CheckingAccount implements HasMenu{
+	//Sotes the current balance for this checking account
 	private double balance;
-
+	//Basic constructor with a starting balance of 0.0
 	public CheckingAccount(){
 		this.balance = 0.0;
 	}
+	//Constructor that lets you set a starting balance.
 	public CheckingAccount(double balance){
 		this.balance = balance;
 	}
+	//Main method for the testing the CheckingAccount class
 	public static void main(String[] args){
 		CheckingAccount ca = new CheckingAccount();
-		ca.start();
+		ca.start();//Start menu loop
 	}
+	//Prints my menu options and returns user choice
 	public String menu(){
 		System.out.println("Account menu");
 		System.out.println("0) Quit");
@@ -23,6 +27,7 @@ public class CheckingAccount implements HasMenu{
 		Scanner sc = new Scanner(System.in);
 		return sc.nextLine();
 	}
+	//Main loop for the menu until user enters 0
 	public void start(){
 		while (true){
 			String choice = menu();
@@ -42,6 +47,7 @@ public class CheckingAccount implements HasMenu{
 	public double getBalance(){
 		return balance;
 	}
+	//Handles deposits prints the new balance 
 	public void makeDeposit(){
 		System.out.println("Making a deposit...");
 		System.out.print("How much would you like to deposit? ");
@@ -49,29 +55,35 @@ public class CheckingAccount implements HasMenu{
 		balance += amt;
 		System.out.println("New balance: " + getBalance());
 	}
+	//Handles withdrawals and prints the new balance.
 	public void makeWithdrawal(){
 		System.out.println("Making a withdrawal...");
 		System.out.print("How much would you like to withdraw? ");
-		double amt = getDouble();
-		balance -= amt;
+		double amt = getDouble();//Read the double from the user
+		balance -= amt;//Subtract entered amount
 		System.out.println("New balance: " + getBalance());
 	}
+	//Safely read a double from the user without crashing the program
 	private double getDouble(){
 		Scanner sc = new Scanner(System.in);
 		try {
-			return Double.parseDouble(sc.nextLine());
+			return Double.parseDouble(sc.nextLine());//Attempt to convert the users input into a double.
 		} catch (Exception e) {
+			//If the user types something invalid, return a 0.0
 			return 0.0;
 		}
 	}
 
+	//Formats the balance in money format.
 	public String getBalanceString(){
 		return String.format("$%.2f", balance);
 	
 	}
+	//Lets the balance be changed manually if needed.
 	public void setBalance(double balance){
 		this.balance = balance;
 	}
+	//Prints the current balance 
 	public void checkBalance(){
 		System.out.println("Checking balance...");
 		System.out.println("Current balance: " + getBalanceString());
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Bank-on-it
+Algorithm/Pseudocode
+```
+Create a CheckingAccount object with a default balance of 0.0.
+    Start the main loop for the menu
+    Create the menu that the user with use to initialize commands.
+    Read the user choice as a string.
+        If the user selects 0:
+            Quit the program and end the loop. 
+        else if the user enters 1:
+            Check the balance (Currently just a mutable double.)
+            Format the balance using getBalanceString()
+            Print the current balance.
+        else if user enters 2:
+            Print ("Making a deposit")
+            System.out.print ("How much would you like to deposit?")
+            Use getDouble() to read the number.
+            Add that amount from the current balance.
+        else if the user enters 3:
+            Print ("Making a withdrawal")
+            System.out.print ("How much would you like to withdrawal?")
+            Use getDouble() to read the number.
+            Subtract that amount from the current balance.
+        else:
+            print ("Invalid choice, please try agian.")
+    Loop until the user enters 0 to exit the loop.
+
+Next week I'll get to add the whole pin, add user, and admin systems.
