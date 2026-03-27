e3d15536899006c8534912b4d3aa13ec2e6f8710
Joseph Kloepper <Thunderpop57@Gmail.com>
2026-03-27 19:18:53 +0000

    More bank changes

--- Bank.java
+++ Bank.java
@@ -31,7 +31,7 @@ public class Bank implements HasMenu{
 			System.out.print("Action: ");
 			choice = sc.nextInt();
 			if (choice == 1){
-				System.out.println("Admin login pressed");
+				adminLogin();
 			}
 			else if (choice == 2){
 				System.out.println("Login as customer pressed");
@@ -43,5 +43,84 @@ public class Bank implements HasMenu{
 				System.out.println("Invalid input");
 			}
 		}
+	}
+
+	private void adminLogin(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter admin username: ");
+		String user = sc.nextLine();
+
+		System.out.print("Enter admin PIN: ");
+		String pin = sc.nextLine();
+
+		if (user.equals(admin.getUserName()) && pin.equals(admin.getPin())){
+			System.out.println("Login successful.");
+			startAdmin();
+		} else {
+			System.out.println("Admin login failed.");
+		}
+	}
+
+	private void startAdmin(){
+		Scanner sc = new Scanner(System.in);
+		int choice = -1;
+
+		while (choice != 0){
+			System.out.println(admin.menu());
+			System.out.print("Action: ");
+			choice = sc.nextInt();
+			sc.nextLine();
+
+			if (choice == 1){
+				fullCustomerReport();
+			}
+			else if (choice == 2){
+				addUser();
+			}
+			else if (choice == 3){
+				applyInterest();
+			}
+			else if (choice == 0){
+				System.out.println("Exiting to main menu.");
+			}
+			else {
+				System.out.println("Invalid Input.");
+			}
+		}
+	}
+	private void fullCustomerReport(){
+		System.out.println("\n--Full Customer Report--");
+		if (customers.isEmpty()){
+			System.out.println("No customers found.");
+			return;
+		}
+		
+		for (Customer c : customers){
+			System.out.println(c);
+		}
+	}
+	private void addUser(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter customer name: ");
+		String name = sc.nextLine();
+
+		System.out.print("Enter customer ID: ");
+		String id = sc.nextLine();
+
+		Customer c = new Customer(name, id);
+		customers.add(c);
+
+		System.out.println("Customer added succesfully.");
+	}
+
+	private void applyInterest(){
+		if (customers.isEmpty()){
+			System.out.println("No customers to apply interest to.");
+			return;
+		}
+		for (Customer c : customers){
+			c.getSavings().applyInterest();
+		}
+		System.out.println("Interest applied to all savings accounts.");
 	}
 }
