992b1e8426d2e177a13fc333a1e1eeb8959e9236
Joseph Kloepper <Thunderpop57@Gmail.com>
2026-03-27 00:56:21 +0000

    Admin.java file

---
+++ Admin.java
@@ -0,0 +1,24 @@
+//Admin (Extends user)
+
+//Constructor that sets default user name and PIN.
+public class Admin extends User{
+	public Admin(){
+		this.userName = "admin";
+		this.pin = "0000";
+	}
+
+	public String menu(){
+		return "\nAdmin Menu\n"
+			+ "0) Exit\n"
+			+ "1)Full Customer Report\n"
+			+ "2) Add User\n"
+			+ "3) Apply Interest\n";
+	}
+
+	public void start(){
+	}
+
+	public String getReport(){
+		return "Admin username" + this.userName;
+	}
+}
